197e23078d7b56dcb5753b0b68fcbe952f5e289d
foivos47 <foivos2000@gmail.com>
2026-03-09 05:07:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,14 +11,13 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Discord bot is running");
+  res.send("Bot is running");
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Web server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
-
 const { Client, GatewayIntentBits } = require('discord.js');
 
 const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
